69bdd228cea5e63bcf9c5133763707f456cc3520
Harshitha Bobba <harshithabobba@pal-nat186-4-192.itap.purdue.edu>
2019-02-06 18:31:08 -0500

    Initial Commit

---
+++ MarioKart/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MarioKart
+//
+//  Created by Harshitha Bobba on 2/6/19.
+//  Copyright © 2019 Harshitha Bobba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

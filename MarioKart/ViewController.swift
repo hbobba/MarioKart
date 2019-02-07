86949e815f3e87818937cbc701cfe9c4498e3fac
Harshitha Bobba <harshithabobba@harshithas-macbook-pro.local>
2019-02-06 19:22:56 -0500

    Users can move karts around the screen using pan, pinch, and rotation gestures.

--- MarioKart/ViewController.swift
+++ MarioKart/ViewController.swift
@@ -14,7 +14,31 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
 
-
+    @IBAction func didPanKartView(_ sender: UIPanGestureRecognizer) {
+        
+        let location = sender.location(in: view)
+        print("Location: x: \(location.x), y \(location.y)")
+        let kartView = sender.view!
+        kartView.center = location
+    }
+    
+    @IBAction func didPinchKartView(_ sender: UIPinchGestureRecognizer) {
+        
+        let scale = sender.scale
+        print("scale: \(scale)")
+        let kartView = sender.view!
+        kartView.transform = CGAffineTransform(scaleX: scale, y: scale)
+        
+        
+    }
+    @IBAction func didRotateKartView(_ sender: UIRotationGestureRecognizer) {
+        
+        let rotation = sender.rotation
+        print("rotation: \(rotation)")
+        let kartView = sender.view!
+        kartView.transform = CGAffineTransform(rotationAngle: rotation)
+    }
 }
 
